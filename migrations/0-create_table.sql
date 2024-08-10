b1585841942460bb7bbec68bc5cea5dad9b2bbc0
PonponJuice <ponjuice2021@gmail.com>
2024-08-10 21:23:28 +0900

    仮のdatabase

---
+++ migrations/0-create_table.sql
@@ -0,0 +1,71 @@
+CREATE TABLE IF NOT EXISTS `user` (
+	`user_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`username` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255),
+	`traq_id` VARCHAR(255),
+	`github_id` VARCHAR(255),
+	`icon_url` VARCHAR(255),
+	`submit_count` INT DEFAULT 0,
+	`post_problem_count` INT DEFAULT 0,
+	`role` VARCHAR(255),
+	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `user_password` (
+	`user_id` INT PRIMARY KEY,
+	`hashed_pass` VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS `normal_problem` (
+	`problem_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`auther_id` INT NOT NULL,
+	`statement` TEXT NOT NULL,
+	`time_limit` INT NOT NULL,
+	`memory_limit` INT NOT NULL,
+	`difficulty` INT NOT NULL,
+	`is_public` BOOLEAN DEFAULT FALSE,
+	`judgecode_path` VARCHAR(255) NOT NULL,
+    `publish_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `testcase` (
+	`testcase_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`problem_id` INT NOT NULL,
+	`testcase_name` VARCHAR(255) NOT NULL,
+	`testcase_path` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `editiorial` (
+	`editorial_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`problem_id` INT NOT NULL,
+	`auther_id` INT NOT NULL,
+	`statement` TEXT NOT NULL,
+	`publish_time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`updated_time` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `submission` (
+	`submission_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`problem_id` INT NOT NULL,
+	`auther_id` INT NOT NULL,
+	`language_id` INT NOT NULL,
+	`code` TEXT NOT NULL,
+	`judge_status` VARCHAR(4) NOT NULL,
+	`total_score` BIGINT NOT NULL,
+	`max_time` INT NOT NULL,
+	`max_memory` INT NOT NULL,
+	`submitted_at` DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `submission_testcase` (
+	`testcase_id` INT AUTO_INCREMENT PRIMARY KEY,
+	`submission_id` INT NOT NULL,
+	`testcase_name` VARCHAR(255) NOT NULL,
+	`judge_status` VARCHAR(4) NOT NULL,
+	`score` BIGINT NOT NULL,
+	`time` INT NOT NULL,
+	`memory` INT NOT NULL
+);
