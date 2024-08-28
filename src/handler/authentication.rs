4fa50a215d4092e342bf047dbea16272822f7232
PonponJuice <ponjuice2021@gmail.com>
2024-08-28 17:00:14 +0900

    メール送信部分の作成

--- src/handler/authentication.rs
+++ src/handler/authentication.rs
@@ -1,4 +1,9 @@
 use axum::{extract::State, Json};
+use lettre::{
+    transport::smtp::authentication::Credentials, Message,
+    message::{header, SinglePart},
+    SmtpTransport, Transport, message::Mailbox, Address
+};
 use reqwest::StatusCode;
 use serde::Deserialize;
 
@@ -15,15 +20,42 @@ pub async fn sign_up_request(
     State(state): State<Repository>,
     Json(body): Json<SignUpRequest>,
 ) -> Result<StatusCode, StatusCode> {
+    let user_address = body.email.parse::<Address>().map_err(|_| StatusCode::BAD_REQUEST)?;
 
+    let app_address = std::env::var("MAIL_ADDRESS").unwrap();
+    let app_password = std::env::var("MAIL_PASSWORD").unwrap();
+    let smtp = "smtp.gmail.com";
     let jwt = state
         .make_jwt_and_save(&body.email)
         .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let message = format!("これはテストメールです。
+    以下のリンクをクリックしてください。
+    https://link/{jwt}");
+
+
+    let email = Message::builder()
+        .from(Mailbox::new(Some("traOJudge".to_owned()), app_address.parse::<Address>().unwrap()))
+        .to(Mailbox::new(None, user_address))
+        .subject("テストメール")
+        .singlepart(
+            SinglePart::builder()
+                .header(header::ContentType::TEXT_PLAIN)
+                .body(message.to_owned()),
+        )
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let credentials = Credentials::new(app_address, app_password);
+
+
+    let mailer = SmtpTransport::starttls_relay(smtp)
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        .ok_or(StatusCode::BAD_REQUEST)?;
+        .credentials(credentials)
+        .build();
 
-    // メール送る
-    
-
-    Ok(StatusCode::CREATED)
+    match mailer.send(&email) {
+        Ok(_) => Ok(StatusCode::CREATED),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
 }
--- src/repository/signup_jwt.rs
+++ src/repository/signup_jwt.rs
@@ -3,7 +3,7 @@ use super::Repository;
 
 
 impl Repository {
-    pub async fn make_jwt_and_save(&self, email: &str) -> anyhow::Result<Option<String>> {
-        Ok(Some(email.to_owned()))
+    pub async fn make_jwt_and_save(&self, email: &str) -> anyhow::Result<String> {
+        Ok(email.to_owned())
     }
 }
