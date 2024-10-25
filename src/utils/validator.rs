f1725c27cb1cc0da49a45ac48ecc12b703892cf8
kenken714 <kenkenkeiko043@gmail.com>
2024-10-25 23:27:07 +0900

    feat: add validator trait

---
+++ src/utils/validator.rs
@@ -0,0 +1,38 @@
+pub mod rules;
+
+pub enum RuleType {
+    Email,
+    UserName,
+    Password,
+    Icon,
+    XLink,
+    GitHubLink,
+    SelfIntroduction,
+}
+
+impl RuleType {
+    fn get_rule(&self) -> &'static str {
+        match self {
+            RuleType::Email => rules::EMAIL_RULE,
+            RuleType::UserName => rules::USER_NAME_RULE,
+            RuleType::Password => rules::PASSWORD_RULE,
+            RuleType::Icon => rules::ICON_RULE,
+            RuleType::XLink => rules::X_LINK_RULE,
+            RuleType::GitHubLink => rules::GITHUB_LINK_RULE,
+            RuleType::SelfIntroduction => rules::SELF_INTRODUCTION_RULE,
+        }
+    }
+    pub fn validate(&self, value: &str) -> anyhow::Result<()> {
+        let rule = self.get_rule();
+        let re = regex::Regex::new(rule)?;
+        if re.is_match(value) {
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!("Invalid value"))
+        }
+    }
+}
+
+pub trait Validator {
+    fn validate(&self) -> anyhow::Result<()>;
+}
