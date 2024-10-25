9c397e7cd200993eb4dad259865d64c85a37fcc4
kenken714 <kenkenkeiko043@gmail.com>
2024-10-26 01:05:48 +0900

    feat impl UserId and add create_user

--- src/repository/users.rs
+++ src/repository/users.rs
@@ -1,5 +1,10 @@
+use std::fmt;
+
 use serde::Serialize;
-use sqlx::{types::chrono, FromRow};
+use sqlx::FromRow;
+use sqlx::Type;
+use sqlx::{types::chrono, Decode, Encode, MySql};
+use uuid::{ContextV7, Timestamp, Uuid};
 
 use super::Repository;
 
@@ -12,10 +17,15 @@ pub enum UserRole {
     admin = 2,
 }
 
-#[derive(Debug, FromRow, Serialize)]
+#[derive(Debug, FromRow, Serialize, Clone, Copy, PartialEq, Eq, Hash)]
+#[serde(transparent)]
+pub struct UserId(Uuid);
+
+#[derive(Debug, FromRow, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
-    pub id: i64,
+    pub id: UserId,
+    pub display_id: i64,
     pub name: String,
     pub traq_id: Option<String>,
     pub github_id: Option<String>,
@@ -39,25 +49,96 @@ pub struct UpdateUser {
     pub self_introduction: String,
 }
 
+impl UserId {
+    pub fn new(id: Uuid) -> Self {
+        Self(id)
+    }
+}
+
+impl From<Uuid> for UserId {
+    fn from(id: Uuid) -> Self {
+        Self(id)
+    }
+}
+
+impl From<UserId> for Uuid {
+    fn from(id: UserId) -> Self {
+        id.0
+    }
+}
+
+impl fmt::Display for UserId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<'a> Decode<'a, MySql> for UserId {
+    fn decode(
+        value: <MySql as sqlx::database::HasValueRef<'a>>::ValueRef,
+    ) -> Result<Self, sqlx::error::BoxDynError> {
+        <Uuid as Decode<'a, MySql>>::decode(value).map(UserId)
+    }
+}
+
+impl<'a> Encode<'a, MySql> for UserId {
+    fn encode_by_ref(
+        &self,
+        buf: &mut <MySql as sqlx::database::HasArguments<'a>>::ArgumentBuffer,
+    ) -> sqlx::encode::IsNull {
+        self.0.encode_by_ref(buf)
+    }
+
+    fn encode(
+        self,
+        buf: &mut <MySql as sqlx::database::HasArguments<'a>>::ArgumentBuffer,
+    ) -> sqlx::encode::IsNull
+    where
+        Self: Sized,
+    {
+        self.0.encode(buf)
+    }
+}
+
+impl Type<MySql> for UserId {
+    fn type_info() -> sqlx::mysql::MySqlTypeInfo {
+        <Uuid as Type<MySql>>::type_info()
+    }
+}
+
 impl Repository {
-    pub async fn get_user_by_id(&self, user_id: i64) -> anyhow::Result<Option<User>> {
-        let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = ?")
-            .bind(user_id)
+    pub async fn get_user_by_display_id(&self, display_id: i64) -> anyhow::Result<Option<User>> {
+        let user = sqlx::query_as::<_, User>("SELECT * FROM users WHERE display_id = ?")
+            .bind(display_id)
             .fetch_optional(&self.pool)
             .await?;
 
         Ok(user)
     }
 
-    pub async fn update_user(&self, user_id: i64, body: UpdateUser) -> anyhow::Result<()> {
-        sqlx::query("UPDATE users SET name = ?, icon_url = ?, x_link = ?, github_link = ?, self_introduction = ? WHERE id = ?")
+    pub async fn create_user(&self, name: String) -> anyhow::Result<UserId> {
+        let ts = Timestamp::now(ContextV7::new());
+        let id = UserId::new(Uuid::new_v7(ts));
+
+        sqlx::query("INSERT INTO users (id, name) VALUES (?, ?)")
+            .bind(id)
+            .bind(name)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(id)
+    }
+
+    pub async fn update_user(&self, display_id: i64, body: UpdateUser) -> anyhow::Result<()> {
+        sqlx::query("UPDATE users SET name = ?, icon_url = ?, x_link = ?, github_link = ?, self_introduction = ? WHERE display_id = ?")
             .bind(body.user_name)
             .bind(body.icon_url)
             .bind(body.x_link)
             .bind(body.github_link)
             .bind(body.self_introduction)
-            .bind(user_id)
+            .bind(display_id)
             .execute(&self.pool).await?;
+
         Ok(())
     }
 }
