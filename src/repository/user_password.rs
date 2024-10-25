f9ee0684fe5e9e4e63efdf7f0c98c0bc36344624
kenken714 <kenkenkeiko043@gmail.com>
2024-10-26 01:03:36 +0900

    feat: add save_row_password

---
+++ src/repository/user_password.rs
@@ -0,0 +1,16 @@
+use super::{users::UserId, Repository};
+
+impl Repository {
+    pub async fn save_raw_password(&self, id: UserId, password: &str) -> anyhow::Result<()> {
+        let hash = bcrypt::hash(password, self.bcrypt_cost)?;
+
+        sqlx::query("INSERT INTO user_passwords (user_id, password) VALUES (?, ?)")
+            .bind(id)
+            .bind(&hash)
+            .bind(&hash)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(())
+    }
+}
