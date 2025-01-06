2822227d677e9e908c0f1e5eec48794f0d1c20bc
PonponJuice <ponjuice2021@gmail.com>
2025-01-07 01:46:01 +0900

    add minio and s3_client code

--- src/repository.rs
+++ src/repository.rs
@@ -1,4 +1,6 @@
 use async_sqlx_session::MySqlSessionStore;
+use aws_config::Region;
+use aws_sdk_s3::{config::Credentials, Config};
 use sqlx::mysql::{MySqlConnectOptions, MySqlPoolOptions};
 
 use super::Repository;
@@ -20,14 +22,21 @@ impl Repository {
         let session_store =
             MySqlSessionStore::from_client(pool.clone()).with_table_name("user_sessions");
 
+        let config = get_config_from_env()?;
+        let s3_client = aws_sdk_s3::Client::from_conf(config);
+        let bucket_name = std::env::var("OBJECT_STORAGE_BUCKET_NAME")?;
+        let _ = s3_client.create_bucket().bucket(&bucket_name).send().await;
+
         Ok(Self {
             pool,
             session_store,
+            s3_client,
+            bucket_name,
             bcrypt_cost: bcrypt::DEFAULT_COST,
         })
     }
 
-    pub async fn migrate(&self) -> sqlx::Result<()> {
+    pub async fn migrate(&self) -> anyhow::Result<()> {
         sqlx::migrate!("./migrations").run(&self.pool).await?;
 
         self.session_store.migrate().await?;
@@ -40,9 +49,16 @@ impl Repository {
             MySqlSessionStore::from_client(pool.clone()).with_table_name("user_sessions");
         session_store.migrate().await?;
 
+        let config = get_config_from_env()?;
+        let s3_client = aws_sdk_s3::Client::from_conf(config);
+        let bucket_name = std::env::var("OBJECT_STORAGE_BUCKET_NAME")?;
+        let _ = s3_client.create_bucket().bucket(&bucket_name).send().await;
+
         Ok(Self {
             pool,
             session_store,
+            s3_client,
+            bucket_name,
             bcrypt_cost: bcrypt::DEFAULT_COST,
         })
     }
@@ -63,4 +79,22 @@ fn get_option_from_env() -> anyhow::Result<MySqlConnectOptions> {
         .username(&user)
         .password(&password)
         .database(&db_name))
+}
+
+fn get_config_from_env() -> anyhow::Result<Config> {
+    let access_key = std::env::var("OBJECT_STORAGE_ACCESS_KEY")?;
+    let secret_key = std::env::var("OBJECT_STORAGE_SECRET_KEY")?;
+    let region = std::env::var("OBJECT_STORAGE_REGION")?;
+    let endpoint = std::env::var("OBJECT_STORAGE_ENDPOINT")?;
+
+    let credentials_provider = Credentials::new(access_key, secret_key, None, None, "static");
+    let config = aws_sdk_s3::Config::builder()
+        .behavior_version_latest()
+        .credentials_provider(credentials_provider)
+        .region(Region::new(region))
+        .force_path_style(true)
+        .endpoint_url(endpoint)
+        .build();
+
+    Ok(config)
 }
