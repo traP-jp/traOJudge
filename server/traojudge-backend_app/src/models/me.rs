a18394f8b049539660490ff75c7d274e2712c99e
PonponJuice <ponjuice2021@gmail.com>
2026-06-16 17:38:41 +0900

    Revert "ユーザーの役割をグローバル権限属性に置き換え、関連する構造体を追加。役割に関するコードを削除。"
    This reverts commit 195a1a3253457b531fd0cdc8a9031b12cef92e17.

--- server/traojudge-backend_app/src/models/me.rs
+++ server/traojudge-backend_app/src/models/me.rs
@@ -36,8 +36,8 @@ pub struct Me {
     pub x_id: Option<String>,
     #[serde(rename = "selfIntroduction", skip_serializing_if = "Option::is_none")]
     pub self_introduction: Option<String>,
-    #[serde(rename = "globalAuthorityAttribute")]
-    pub global_authority_attribute: models::UserGlobalAuthorityAttribute,
+    #[serde(rename = "role")]
+    pub role: models::Role,
     #[serde(rename = "createdAt")]
     pub created_at: String,
     #[serde(rename = "updatedAt")]
@@ -51,7 +51,7 @@ impl Me {
     pub fn new(
         id: String,
         name: String,
-        global_authority_attribute: models::UserGlobalAuthorityAttribute,
+        role: models::Role,
         created_at: String,
         updated_at: String,
         authentication: models::UserAuthentication,
@@ -66,7 +66,7 @@ impl Me {
             submit_problems: None,
             x_id: None,
             self_introduction: None,
-            global_authority_attribute,
+            role,
             created_at,
             updated_at,
             authentication,
--- server/traojudge-backend_app/src/models/user.rs
+++ server/traojudge-backend_app/src/models/user.rs
@@ -36,8 +36,8 @@ pub struct User {
     pub x_id: Option<String>,
     #[serde(rename = "selfIntroduction", skip_serializing_if = "Option::is_none")]
     pub self_introduction: Option<String>,
-    #[serde(rename = "globalAuthorityAttribute")]
-    pub global_authority_attribute: models::UserGlobalAuthorityAttribute,
+    #[serde(rename = "role")]
+    pub role: models::Role,
     #[serde(rename = "createdAt")]
     pub created_at: String,
     #[serde(rename = "updatedAt")]
@@ -49,7 +49,7 @@ impl User {
     pub fn new(
         id: String,
         name: String,
-        global_authority_attribute: models::UserGlobalAuthorityAttribute,
+        role: models::Role,
         created_at: String,
         updated_at: String,
     ) -> User {
@@ -63,7 +63,7 @@ impl User {
             submit_problems: None,
             x_id: None,
             self_introduction: None,
-            global_authority_attribute,
+            role,
             created_at,
             updated_at,
         }
