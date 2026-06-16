818b897476def0c606571282f14eb16d3a7f79ef
PonponJuice <ponjuice2021@gmail.com>
2026-06-16 15:52:14 +0900

    refactor: update Problem model and repository to use i64 for ProblemId and unify naming conventions

--- server/traojudge-core/src/domain/model/problem.rs
+++ server/traojudge-core/src/domain/model/problem.rs
@@ -2,40 +2,39 @@ use super::user::UserId;
 use anyhow::Context;
 use chrono::{DateTime, Utc};
 use std::str::FromStr;
-use uuid::Uuid;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ProblemId(Uuid);
+pub struct ProblemId(i64);
 
 impl FromStr for ProblemId {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let id = Uuid::parse_str(s).context("failed to parse ProblemId from str")?;
+        let id = s.parse().context("failed to parse ProblemId from str")?;
         Ok(ProblemId(id))
     }
 }
 
-impl From<Uuid> for ProblemId {
-    fn from(id: Uuid) -> Self {
+impl From<i64> for ProblemId {
+    fn from(id: i64) -> Self {
         Self(id)
     }
 }
 
-impl Into<Uuid> for ProblemId {
-    fn into(self) -> Uuid {
-        self.0
+impl From<ProblemId> for i64 {
+    fn from(id: ProblemId) -> Self {
+        id.0
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct NormalProblem {
+pub struct Problem {
     pub id: ProblemId,
-    pub author_id: UserId,
+    pub author_user_id: UserId,
     pub title: String,
     pub statement: String,
     pub time_limit_ms: i32,
-    pub memory_limit_kib: i32,
+    pub memory_limit_mib: i32,
     pub difficulty: i32,
     pub is_public: bool,
     pub solved_count: i32,
@@ -43,21 +42,21 @@ pub struct NormalProblem {
     pub updated_at: DateTime<Utc>,
 }
 
-pub struct UpdateNormalProblem {
+pub struct UpdateProblem {
     pub title: String,
     pub is_public: bool,
     pub difficulty: i32,
     pub statement: String,
     pub time_limit_ms: i32,
-    pub memory_limit_kib: i32,
+    pub memory_limit_mib: i32,
 }
 
-pub struct CreateNormalProblem {
-    pub author_id: UserId,
+pub struct CreateProblem {
+    pub author_user_id: UserId,
     pub title: String,
     pub statement: String,
     pub time_limit_ms: i32,
-    pub memory_limit_kib: i32,
+    pub memory_limit_mib: i32,
     pub difficulty: i32,
 }
 
--- server/traojudge-core/src/domain/repository/problem.rs
+++ server/traojudge-core/src/domain/repository/problem.rs
@@ -2,16 +2,15 @@ use anyhow::Result;
 use async_trait::async_trait;
 
 use crate::domain::model::problem::{
-    CreateNormalProblem, NormalProblem, ProblemGetQuery, ProblemId, UpdateNormalProblem,
+    CreateProblem, Problem, ProblemGetQuery, ProblemId, UpdateProblem,
 };
 
 #[async_trait]
 pub trait ProblemRepository: Send {
-    async fn get_problem(&mut self, id: ProblemId) -> Result<Option<NormalProblem>>;
-    async fn get_problems_by_query(&mut self, query: ProblemGetQuery)
-    -> Result<Vec<NormalProblem>>;
+    async fn get_problem(&mut self, id: ProblemId) -> Result<Option<Problem>>;
+    async fn get_problems_by_query(&mut self, query: ProblemGetQuery) -> Result<Vec<Problem>>;
     async fn get_problems_by_query_count(&mut self, query: ProblemGetQuery) -> Result<i64>;
-    async fn create_problem(&mut self, problem: CreateNormalProblem) -> Result<ProblemId>;
-    async fn update_problem(&mut self, id: ProblemId, problem: UpdateNormalProblem) -> Result<()>;
+    async fn create_problem(&mut self, problem: CreateProblem) -> Result<ProblemId>;
+    async fn update_problem(&mut self, id: ProblemId, problem: UpdateProblem) -> Result<()>;
     async fn delete_problem(&mut self, id: ProblemId) -> Result<()>;
 }
