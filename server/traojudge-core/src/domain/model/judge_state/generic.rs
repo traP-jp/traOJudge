eb0ba11ec17a7a51efa005144be70bd9bc88c80a
comavius <comavius@proton.me>
2026-06-17 18:12:12 +0900

    add dynamic files for judge run state

--- server/traojudge-core/src/domain/model/judge_state/generic.rs
+++ server/traojudge-core/src/domain/model/judge_state/generic.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
-use crate::domain::model::{dag::generic_dag::NodeId, submission::JudgeRunId};
+use crate::domain::model::{dag::generic_dag::NodeId, file::StoreFileId, submission::JudgeRunId};
 
 pub struct JudgeRunState {
     pub judge_run_id: JudgeRunId,
     pub dynamic_env_vars: HashMap<String, String>,
+    pub dynamic_files: HashMap<String, StoreFileId>,
     pub nodes: JudgeRunNode,
 }
 
